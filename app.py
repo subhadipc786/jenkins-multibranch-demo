4b6f3206b42d2cf116d7403a04d33b02cb29b859
subhadipc786 <63358238+subhadipc786@users.noreply.github.com>
2025-10-22 04:43:26 +0530

    Add development version print statement

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+
 # app.py - main branch
 # This is a simple placeholder application.
 # You can use it to test Jenkins Multibranch Pipeline builds.
 
 print("Hello from the MAIN branch — production version!")
+print("Adding new line for DEV - development version")
