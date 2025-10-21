c6b4701417140bc8d4ce35f8738e33f6fdddc182
subhadipc786 <63358238+subhadipc786@users.noreply.github.com>
2025-10-22 04:37:30 +0530

    Add placeholder application in app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+# app.py - main branch
+# This is a simple placeholder application.
+# You can use it to test Jenkins Multibranch Pipeline builds.
+
+print("Hello from the MAIN branch — production version!")
